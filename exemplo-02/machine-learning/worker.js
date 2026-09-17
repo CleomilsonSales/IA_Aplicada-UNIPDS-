5236d6f1d98d824d524e5e5880a83374322ea350
Cleomilson Sales <cleomilsonsales@hotmail.com>
2026-09-16 22:00:00 -0300

    fim do modulo 6

---
+++ exemplo-02/machine-learning/worker.js
@@ -0,0 +1,117 @@
+importScripts('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest');
+
+const MODEL_PATH = `yolov5n_web_model/model.json`;
+const LABELS_PATH = `yolov5n_web_model/labels.json`;
+const INPUT_MODEL_DIMENTIONS = 640;
+const CLASS_THRESHOLD = 0.4;
+
+let _label = [];
+let _model = null;
+
+async function loadModelAndLabel(){
+  await tf.ready();
+
+  _label = await(await fetch(LABELS_PATH)).json();
+  _model = await(await tf.loadGraphModel(MODEL_PATH));
+
+  //warmup
+  const dummyInput = tf.ones(_model.inputs[0].shape);
+
+  await _model.executeAsync(dummyInput);
+
+  postMessage({ type: 'model-loaded'});
+
+
+}
+
+/**
+ * tidy garante que os tensores temporarios serão descartados automaticamente
+ * evintando vazamento de memoria
+ *
+ * fromPixels converte imagemBitmap/ImageData [H,W,3]
+ * div(255) normaliza os valores para [0,1]
+ * expandDims(0) adiciona dimensão batch [1,H,W,3]
+ */
+function preprocessImage(input){
+  return tf.tidy(() => {
+    const image = tf.browser.fromPixels(input);
+
+    return tf.image
+      .resizeBilinear(image, [INPUT_MODEL_DIMENTIONS, INPUT_MODEL_DIMENTIONS])
+      .div(255)
+      .expandDims(0);
+  });
+}
+
+async function runInference(tensor){
+  const output = await _model.executeAsync(tensor);
+  tf.dispose(tensor);
+
+  const [boxes, scores, classes] = output.slice(0, 3);
+  const [boxesData, scoresData, classesData] = await Promise.all(
+    [
+      boxes.data(),
+      scores.data(),
+      classes.data(),
+    ]
+  );
+
+  output.forEach((t) => t.dispose());
+
+  return {
+    boxes: boxesData,
+    scores: scoresData,
+    classes: classesData
+  };
+}
+
+function *processPrediction({ boxes, scores, classes }, width, height){
+  for (let index = 0; index < scores.length; index++){
+    if (scores[index] < CLASS_THRESHOLD) continue;
+
+    const label =  _label[classes[index]];
+    if (label !== 'kite') continue;
+
+    //*4 pq é para pegar as quatros posições
+    let [x1, y1, x2, y2] = boxes.slice(index * 4, (index + 1) * 4);
+
+    //para ajustar a tela como ela estiver, ja que o modelo é apenas 640x640
+    x1 *= width;
+    x2 *= width;
+    y1 *= height;
+    y2 *= height;
+
+    const boxWidth = x2 - x1;
+    const boxHeigth = y2 - y1;
+    const centerX = x1 + boxWidth / 2;
+    const centerY = y1 + boxHeigth / 2;
+
+    yield {
+      x: centerX,
+      y: centerY,
+      score: (scores[index] * 100).toFixed(2)
+    };
+
+  }
+}
+
+loadModelAndLabel();
+
+
+self.onmessage = async({ data }) => {
+  if (data.type !== 'predict') return;
+  if (!_model) return;
+
+  const input = preprocessImage(data.image);
+  const { width, height} = data.image;
+
+  const inferenceResults = await runInference(input);
+
+  for (const prediction of  processPrediction(inferenceResults, width, height)){
+    postMessage({
+      type: 'prediction',
+      ...prediction
+    });
+  }
+
+};
